de219691500751be7a2a1edbe5991332873b878b
Mateus de Sena Lima <mtssena.lima@gmail.com>
2024-05-20 18:35:41 -0300

    Update CRUD.py

--- CRUDPYTHON/CRUD.py
+++ CRUDPYTHON/CRUD.py
@@ -1,1 +1,33 @@
 import mysql.connector
+
+#crie sua conexao com o BD (precisa do host, user, password e o database )
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='bdcrud',
+)
+
+#crie o seu "cursor" (ele é responsável por executar os comandos)
+cursor = conexao.cursor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#encerrando a conexão
+cursor.close()
+conexao.close(  )
