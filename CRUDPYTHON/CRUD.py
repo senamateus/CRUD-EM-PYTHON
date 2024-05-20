7ef11b69eab3a9ca06b05c27e663996182d18936
Mateus de Sena Lima <mtssena.lima@gmail.com>
2024-05-20 20:41:35 -0300

    Update CRUD.py

--- CRUDPYTHON/CRUD.py
+++ CRUDPYTHON/CRUD.py
@@ -18,6 +18,13 @@ cursor.execute(comando)
 resultado = cursor.fetchall()
 print(resultado)
 
+#UPDATE
+nome_produto = "chocolate"
+valor = 8
+comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+cursor.execute(comando)
+conexao.commit()
+
 
 
 
