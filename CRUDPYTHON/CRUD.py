5986d85fabb5333eaf0a1fcd48ab96cf798d6f60
Mateus de Sena Lima <mtssena.lima@gmail.com>
2024-05-20 20:14:18 -0300

    Update CRUD.py

--- CRUDPYTHON/CRUD.py
+++ CRUDPYTHON/CRUD.py
@@ -8,7 +8,7 @@ cursor = conexao.cursor()
 #CREATE
 nome_produto ="bola"
 valor = 15
-comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}"   , {valor})'
+comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
 cursor.execute(comando)
 conexao.commit()
 
