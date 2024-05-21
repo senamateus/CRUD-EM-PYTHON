6957cdd2e4b1244ecd46224ba1b7ee4a8aec4127
Mateus de Sena Lima <mtssena.lima@gmail.com>
2024-05-21 16:27:03 -0300

    Update CRUD.py

--- CRUDPYTHON/CRUD.py
+++ CRUDPYTHON/CRUD.py
@@ -4,24 +4,23 @@ from flask import Flask, render_template, request, redirect;
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mateusSena'
 
-#crie sua conexao com o BD (precisa do host, user, password e o database )
-conexao = mysql.connector.connect(host='localhost', database='bdcrud', user='root', password='')
-#crie sua conexao com o "cursor" (ele é responsável por executar os comandos)
-cursor = conexao.cursor()
-
-
 
 @app.route('/')
 def index():
    return render_template('index.html')
 
+#CREATE
 @app.route('/paginacreate') 
 def paginacreate():
     return render_template('create.html')
 
 @app.route('/create', methods=['POST'])
 def create():
-    #CREATE
+    #crie sua conexao com o BD (precisa do host, user, password e o database ):    
+    conexao = mysql.connector.connect(host='localhost', database='bdcrud', user='root', password='')
+    #crie sua conexao com o "cursor" (ele é responsável por executar os comandos):
+    cursor = conexao.cursor()
+
     nome_produto = request.form.get('nomeProduto')
     valor = request.form.get('precoProduto')
     comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
